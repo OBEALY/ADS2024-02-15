28ceadb063bbea52b7cb27a5e32416492f23a56e
OBEALY <bymer168@mail.ru>
2024-06-03 18:24:46 +0300

    .

--- src/by/it/group351004/loginov/lesson02/A_VideoRegistrator.java
+++ src/by/it/group351004/loginov/lesson02/A_VideoRegistrator.java
@@ -13,19 +13,50 @@ import java.util.List;
 public class A_VideoRegistrator {
 
     public static void main(String[] args) {
-        A_VideoRegistrator instance = new A_VideoRegistrator();
-        double[] events = new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
-        List<Double> starts = instance.calcStartTimes(events, 1); //рассчитаем моменты старта, с длинной сеанса 1
+        A_VideoRegistrator instance=new A_VideoRegistrator();
+        double[] events=new double[]{1, 1.1, 1.6, 2.2, 2.4, 2.7, 3.9, 8.1, 9.1, 5.5, 3.7};
+        List<Double> starts=instance.calcStartTimes(events,1); //рассчитаем моменты старта, с длинной сеанса 1
         System.out.println(starts);                            //покажем моменты старта
     }
-
     //модификаторы доступа опущены для возможности тестирования
-    List<Double> calcStartTimes(double[] events, double workDuration) {
+
+    double[] QSort(double[] events, int lft, int rght){
+        double crl = events[(lft + rght) / 2];
+        int j=lft, k=rght;
+        while(j < k){
+            while(events[j] < crl) ++j;
+            while(events[k] > crl) --k;
+            if(j<=k){
+                double temp = events[j];
+                events[j] = events[k];
+                events[k] = temp;
+                ++j;
+                --k;
+            }
+        }
+        if(lft < k)
+            events = QSort(events,lft,k);
+        if(j < rght)
+            events = QSort(events,j,rght);
+        return events;
+    }
+
+    List<Double> calcStartTimes(double[] events, double workDuration){
         //events - события которые нужно зарегистрировать
         //timeWorkDuration время работы видеокамеры после старта
         List<Double> result;
         result = new ArrayList<>();
-        int i = 0;                              //i - это индекс события events[i]
+        int i=0;
+
+        events = QSort(events, 0, events.length - 1);
+
+        while (i < events.length){
+            double start = events[i];
+            result.add(start);
+            while ((i<events.length) && (events[i] <= start + workDuration))
+                ++i;
+        }
+        //i - это индекс события events[i]
         //Комментарии от проверочного решения сохранены для подсказки, но вы можете их удалить.
         //Подготовка к жадному поглощению массива событий
         //hint: сортировка Arrays.sort обеспечит скорость алгоритма
@@ -37,6 +68,8 @@ public class A_VideoRegistrator {
         //вычислим момент окончания работы видеокамеры
         //и теперь пропустим все покрываемые события
         //за время до конца работы, увеличивая индекс
+
+
 
 
         return result;                        //вернем итог
--- src/by/it/group351004/loginov/lesson02/B_Sheduler.java
+++ src/by/it/group351004/loginov/lesson02/B_Sheduler.java
@@ -9,34 +9,8 @@ import java.util.List;
 Алгоритм жадный. Для реализации обдумайте надежный шаг.
 */
 
+
 public class B_Sheduler {
-    public static void main(String[] args) {
-        B_Sheduler instance = new B_Sheduler();
-        Event[] events = {new Event(0, 3), new Event(0, 1), new Event(1, 2), new Event(3, 5),
-                new Event(1, 3), new Event(1, 3), new Event(1, 3), new Event(3, 6),
-                new Event(2, 7), new Event(2, 3), new Event(2, 7), new Event(7, 9),
-                new Event(3, 5), new Event(2, 4), new Event(2, 3), new Event(3, 7),
-                new Event(4, 5), new Event(6, 7), new Event(6, 9), new Event(7, 9),
-                new Event(8, 9), new Event(4, 6), new Event(8, 10), new Event(7, 10)
-        };
-
-        List<Event> starts = instance.calcStartTimes(events, 0, 10);  //рассчитаем оптимальное заполнение аудитории
-        System.out.println(starts);                                 //покажем рассчитанный график занятий
-    }
-
-    List<Event> calcStartTimes(Event[] events, int from, int to) {
-        //Events - события которые нужно распределить в аудитории
-        //в период [from, int] (включительно).
-        //оптимизация проводится по наибольшему числу непересекающихся событий.
-        //Начало и конец событий могут совпадать.
-        List<Event> result;
-        result = new ArrayList<>();
-        //ваше решение.
-
-
-        return result;          //вернем итог
-    }
-
     //событие у аудитории(два поля: начало и конец)
     static class Event {
         int start;
@@ -49,7 +23,65 @@ public class B_Sheduler {
 
         @Override
         public String toString() {
-            return "(" + start + ":" + stop + ")";
+            return "("+ start +":" + stop + ")";
         }
+    }
+
+    public static void main(String[] args) {
+        B_Sheduler instance = new B_Sheduler();
+        Event[] events = {  new Event(0, 3),  new Event(0, 1), new Event(1, 2), new Event(3, 5),
+                new Event(1, 3),  new Event(1, 3), new Event(1, 3), new Event(3, 6),
+                new Event(2, 7),  new Event(2, 3), new Event(2, 7), new Event(7, 9),
+                new Event(3, 5),  new Event(2, 4), new Event(2, 3), new Event(3, 7),
+                new Event(4, 5),  new Event(6, 7), new Event(6, 9), new Event(7, 9),
+                new Event(8, 9),  new Event(4, 6), new Event(8, 10), new Event(7, 10)
+        };
+
+        List<Event> starts = instance.calcStartTimes(events,0,10);  //рассчитаем оптимальное заполнение аудитории
+        System.out.println(starts);                                 //покажем рассчитанный график занятий
+    }
+
+    Event[] QSort(Event[] events, int lft, int rght){
+        Event crl = events[(lft + rght) / 2];
+        int j=lft, k=rght;
+        while(j < k){
+            while((events[j].stop < crl.stop) || ((events[j].stop == crl.stop) && (events[j].start < crl.start)))
+                ++j;
+            while((events[k].stop > crl.stop) || ((events[k].stop == crl.stop) && (events[k].start > crl.start)))
+                --k;
+            if(j<=k){
+                Event temp = events[j];
+                events[j] = events[k];
+                events[k] = temp;
+                ++j;
+                --k;
+            }
+        }
+        if(lft < k)
+            events = QSort(events,lft,k);
+        if(j < rght)
+            events = QSort(events,j,rght);
+        return events;
+    }
+
+    List<Event> calcStartTimes(Event[] events, int from, int to) {
+        //Events - события которые нужно распределить в аудитории
+        //в период [from, int] (включительно).
+        //оптимизация проводится по наибольшему числу непересекающихся событий.
+        //Начало и конец событий могут совпадать.
+        List<Event> result;
+        result = new ArrayList<>();
+        //ваше решение.
+        events = QSort(events, 0, events.length - 1);
+
+        int i = 0;
+        while(i < events.length){
+            double stop = events[i].stop;
+            result.add(events[i]);
+            while((i < events.length) && (events[i].start < stop))
+                ++i;
+        }
+
+        return result;          //вернем итог
     }
 }
